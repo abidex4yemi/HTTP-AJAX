62b6175f6991771d904cd2de7de39f926977419a
abidex4yemi <abidex4yemi@gmail.com>
2019-06-12 13:57:45 +0100

    Create getAllFriends method in app.js file

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import uuid from 'uuid';
 import { Route } from 'react-router-dom';
 
@@ -24,15 +25,31 @@ export class App extends Component {
 		};
 	}
 
+	getAllFriends = url => {
+		axios
+			.get(url)
+			.then(res => res.json())
+			.then(res => {
+				this.setState(() => ({ friends: res.dat }));
+			})
+			.catch(err => err)
+			.finally(err => err);
+	};
+
+	componentDidMount() {
+		const url = 'http://localhost:500/friends';
+		this.getAllFriends(url);
+	}
+
 	render() {
 		const { friends } = this.state;
 
 		return (
-			<div>
+			<React.Fragment>
 				{routeDetails.map(({ id, path, ComponentToRender }) => (
 					<Route key={id} exact path={path} render={props => <ComponentToRender {...props} friends={friends} />} />
 				))}
-			</div>
+			</React.Fragment>
 		);
 	}
 }
