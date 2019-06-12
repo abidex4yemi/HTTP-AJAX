e0d728ee4582631ac7ef83d688f3919a20554baf
abidex4yemi <abidex4yemi@gmail.com>
2019-06-12 13:34:59 +0100

    Create Friend component

---
+++ friends/src/components/Friends/Friend.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Friend = props => {
+	const { name, age, email } = props;
+
+	return (
+		<div>
+			<div>
+				<h1>{name}</h1>
+				<h4>{age}</h4>
+				<p>{email}</p>
+			</div>
+		</div>
+	);
+};
+
+Friend.propTypes = {
+	props: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		age: PropTypes.number.isRequired,
+		email: PropTypes.string.isRequired
+	})
+};
