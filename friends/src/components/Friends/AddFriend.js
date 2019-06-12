806783bef98ee14a5f4693c900ddce8e08d98be8
abidex4yemi <abidex4yemi@gmail.com>
2019-06-12 15:30:17 +0100

    Create input change handler

--- friends/src/components/Friends/AddFriend.js
+++ friends/src/components/Friends/AddFriend.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { TextInput } from '../Form/TextInput';
 
 export class AddFriend extends Component {
 	constructor(props) {
@@ -9,13 +10,64 @@ export class AddFriend extends Component {
 			form: {
 				name: '',
 				email: '',
-				age: ''
+				age: '',
+				errors: {}
 			}
 		};
 	}
 
+	inputChange = (field, value) => {
+		if (value.trim() !== '') {
+			this.setState(prevState => {
+				return {
+					form: {
+						...prevState.form,
+						[field]: value
+					}
+				};
+			});
+		}
+	};
+
+	handleInputChange = evt => {
+		const field = evt.target.name;
+		const value = evt.target.value;
+		this.inputChange(field, value);
+	};
+
 	render() {
-		return <form>form goes here</form>;
+		const { name, age, email, errors } = this.props;
+
+		return (
+			<form>
+				<TextInput
+					type="text"
+					value={name}
+					id="name"
+					placeholder="Enter friend name..."
+					error={errors.name}
+					onChange={this.handleInputChange}
+				/>
+
+				<TextInput
+					type="text"
+					value={email}
+					id="email"
+					placeholder="Enter friend email..."
+					error={errors.email}
+					onChange={this.handleInputChange}
+				/>
+
+				<TextInput
+					type="text"
+					value={age}
+					id="age"
+					placeholder="Enter friend age..."
+					error={errors.age}
+					onChange={this.handleInputChange}
+				/>
+			</form>
+		);
 	}
 }
 
