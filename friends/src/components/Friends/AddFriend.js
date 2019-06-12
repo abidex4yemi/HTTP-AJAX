5bb84cbd44f41ccf2f5735fdfb53883dcee475d4
abidex4yemi <abidex4yemi@gmail.com>
2019-06-12 15:11:41 +0100

    Create TextInput component

--- friends/src/components/Friends/AddFriend.js
+++ friends/src/components/Friends/AddFriend.js
@@ -2,27 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class AddFriend extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      friends: this.props,
-      form: {
-        name: '',
-        email: '',
-        age: ''
-      }
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			friends: this.props,
+			form: {
+				name: '',
+				email: '',
+				age: ''
+			}
+		};
+	}
 
-  render() {
-    return(
-      <form>
-        form goes here
-      </form>
-    );
-  }
+	render() {
+		return <form>form goes here</form>;
+	}
 }
 
 AddFriend.propTypes = {
-  friends: PropTypes.arrayOf(PropTypes.object)
-}
+	friends: PropTypes.arrayOf(PropTypes.object)
+};
